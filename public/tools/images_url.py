8df9c91a2527a137aa0ee32131a575512d1c1c7b
Rancho333 <zhwwan@gmail.com>
2022-07-07 10:59:10 +0800

    uptrack db.json and public

--- public/tools/images_url.py
+++
@@ -1,7 +0,0 @@
-# This script can generate the cmd of change images url
-
-os.system("bash get_images.sh")
-from images import images
-
-for i in images:
-    print "find ../posts -name '*.md' | xargs sed -i '/asset_img" + " " + i + "/c![](https://rancho333.gitee.io/pictures/" + i + ".png)'"
